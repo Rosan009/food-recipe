bf9091ef83aa86a60de0b8b56401ea8b97f933cc
carmelpretheesha <carmelpretheesha03@gmail.com>
2024-12-27 12:10:16 +0530

    modify the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
-import Home from './components/Home'; 
+import Home from './components/Home';
 import Nav from './components/Nav';
 import Card from './components/Card';
-import Recipe from './components/Recipe'; // Recipe page component
-import cardData from './components/data'; // Assuming cardData is an array of recipe objects
+import Recipe from './components/Recipe';
+import cardData from './components/data';
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <header>
-          <Nav />
-        </header>
-        <main>
-          {/* Wrap the Routes component around all the Route components */}
-          <Routes>
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/" element={<SignIn />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/recipe/:id" element={<Recipe />} /> {/* Recipe page route */}
-          </Routes>
+  const location = useLocation();
 
-          {/* The Card component should be rendered inside the /home route */}
-          <Routes>
+  // Define routes where the Nav should not appear
+  const noNavRoutes = ['/', '/sign-in', '/sign-up'];
+
+  return (
+    <div>
+      {/* Conditionally render Nav */}
+      {!noNavRoutes.includes(location.pathname) && <Nav />}
+
+      <main>
+        <Routes>
+          {/* Authentication routes */}
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+          {/* Main application routes */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+        </Routes>
+        <Routes>
             <Route path="/home" element={<Card cardData={cardData} />} />
           </Routes>
-        </main>
-      </div>
-    </Router>
+      </main>
+    </div>
   );
 };
 
